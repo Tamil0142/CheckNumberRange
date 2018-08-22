863f178f60ecd0090b9f764026991796d4a15646
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 11:05:05 +0530

    Create CheckNumberRange.java

---
+++ CheckNumberRange.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		if(n>=1 && n<=10)
+			System.out.println("Yes");
+		else
+			System.out.println("No");	
+}}
